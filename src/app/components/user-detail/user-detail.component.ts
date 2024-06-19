07247c33576261022a959c18183c4bab6e2bf327
rubenwest <rubensg90@gmail.com>
2024-06-19 12:05:22 +0200

    feat: add user detail

--- src/app/components/user-detail/user-detail.component.ts
+++ src/app/components/user-detail/user-detail.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { UserDemandant } from '../../models/user-demandant.interface';
+import { UserEmployee } from '../../models/user-employee.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrls: ['./user-detail.component.scss'],
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  user: UserDemandant | UserEmployee | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    const userId = Number(this.route.snapshot.paramMap.get('id'));
+    this.user = this.userService.getUserById(userId);
+  }
 }
