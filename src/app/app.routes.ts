e9b770b3c007b016579ce77c24570f0bbd8056ec
rubenwest <rubensg90@gmail.com>
2024-06-19 10:47:33 +0200

    feat: add fontAwesome and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'list' },
+  { path: 'list', component: UserListComponent },
+];
