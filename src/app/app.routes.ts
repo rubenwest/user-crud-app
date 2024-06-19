55a89c6797cd634b8d57f81e446278e91e9a0973
rubenwest <rubensg90@gmail.com>
2024-06-19 12:04:28 +0200

    feat: add routes and http config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'list' },
   { path: 'list', component: UserListComponent },
+  { path: 'user/new', component: UserFormComponent },
+  { path: 'user/:id', component: UserDetailComponent },
+  { path: 'user/edit/:id', component: UserFormComponent },
 ];
